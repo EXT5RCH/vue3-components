f5eacd8bd4712e525b381d616cd60c58558a5de1
satoshi yamada <satoshi.yamada@sint.co.jp>
2021-11-15 23:57:19 +0900

    [feat]VCheckbox：新規作成

---
+++ src/components/VCheckbox.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { computed } from "@vue/reactivity";
+
+type CheckboxItemType = {
+  label: string;
+  value: string;
+};
+
+interface Props {
+  modelValue: string[];
+  items: CheckboxItemType[];
+  disabled?: boolean;
+}
+
+interface Emits {
+  (e: "update:modelValue", value: string[]): void;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  modelValue: () => [],
+  items: () => [],
+  disabled: false,
+});
+
+const emits = defineEmits<Emits>();
+
+const vmCheckedList = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(newValue: string[]) {
+    emits("update:modelValue", newValue);
+  },
+});
+</script>
+
+<template>
+  <label v-for="i in items" :key="i.value">
+    <input
+      type="checkbox"
+      v-model="vmCheckedList"
+      :value="i.value"
+      :disabled="disabled"
+    />
+    {{ i.label }}
+  </label>
+</template>
---
+++ src/stories/VCheckbox.stories.js
@@ -0,0 +1,40 @@
+import { ref } from "@vue/reactivity";
+import VCheckbox from "../components/VCheckbox.vue";
+
+export default {
+  title: "VCheckbox",
+  component: VCheckbox,
+  argTypes: {
+    disabled: {
+      description: "チェックボックスの使用不可を設定します。",
+    },
+    items: {
+      description: "各チェックボックスの情報を設定します。",
+    },
+  },
+};
+
+const Template = (args) => ({
+  components: { VCheckbox },
+  setup() {
+    const modelValue = ref([]);
+    return { args, modelValue };
+  },
+  template:
+    '<v-checkbox v-bind="args" v-model="modelValue" @update:modelValue="handleChange" />',
+  methods: {
+    handleChange: (e) => {
+      console.log(e);
+    },
+  },
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  items: [
+    { label: "赤", value: "red" },
+    { label: "青", value: "blue" },
+    { label: "黄色", value: "yellow" },
+  ],
+  disabled: false,
+};
