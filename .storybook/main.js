52c41f53257932a31cc80da528342fa87eedb6fe
satoshi yamada <satoshi.yamada@sint.co.jp>
2021-11-21 16:41:08 +0900

    [feat]`sass`追加

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,4 +5,11 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-a11y",
   ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ["style-loader", "css-loader", "sass-loader"],
+    });
+    return config;
+  },
 };
